9e12970b4fe056e90a26fc37cd0161fe903e5886
mayank <geramayank55@gmail.com>
2023-05-06 13:01:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/phone/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.phone.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.phone.R
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
